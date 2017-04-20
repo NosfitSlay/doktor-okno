4e6c10fa60567ba4333fb0e45b7077f4fd3647a9
NosfitSlay <slavikov.net@bk.ru>
2017-04-20 20:07:20 +0300

    PopUp finished
    PopUp finished

--- dist/js/plugin.js
+++ dist/js/plugin.js
@@ -6,16 +6,17 @@
         prevArrow:'<button type="button" class="slick-prev"></button>',
         nextArrow:'<button type="button" class="slick-next"></button>'
     });
+
     $('.orderCallback, .contacts__callBack').on("click", function(e){
         e.preventDefault();
         $(".modalCallBack__openBackground").fadeIn();
         $(".modalCallBack__open").fadeIn();
-    })
-    $('.modalCallBack__openBackground, .modalCallBack__close').on('click',function(){
-        $(".modalCallBack__openBackground").fadeOut();
-        $(".modalCallBack__open").fadeOut();
-    })
+    });
 
+    $('.modalCallBack__openBackground, .modalCallBack__close').on('click',function(){
+       $(".modalCallBack__openBackground").fadeOut();
+       $(".modalCallBack__open").fadeOut();
+    })
 
   });
 
