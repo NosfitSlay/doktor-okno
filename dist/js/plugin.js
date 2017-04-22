c57b71beb51d0bb2a64e407055523c1680d23ccd
NosfitSlay <slavikov.net@bk.ru>
2017-04-22 13:26:53 +0300

    before wordpress

--- dist/js/plugin.js
+++ dist/js/plugin.js
@@ -1,13 +1,15 @@
 
- $(function() {
-    $('.sidebar__slider').slick({
-        autoplay: true,
-        arrows: true,
-        prevArrow:'<button type="button" class="slick-prev"></button>',
-        nextArrow:'<button type="button" class="slick-next"></button>'
-    });
+$(function() {
+	$('.sidebar__slider').slick({
+	    autoplay: true,
+	    arrows: true,
+	    prevArrow:'<button type="button" class="slick-prev"></button>',
+	    nextArrow:'<button type="button" class="slick-next"></button>'
+	});
 
-    $('#menu').slicknav({
-        prependTo:'.demo1'
-    }) 
- });
+	//slick nav
+	$('.headerNav_menu').slicknav({
+		prependTo: '.headerNav',
+	    label: '',
+	}); 
+});
